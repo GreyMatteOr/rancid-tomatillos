c0fe046ae5471f4d6db7573c60cd427c6e1f3d74
Matthew Lane <themplane@gmail.com>
2020-10-07 14:15:58 -0700

    Split up `App` into more components to make it easier to work with

--- src/App.js
+++ src/App.js
@@ -5,34 +5,101 @@ import './App.css';
 import './MovieCard.css';
 import './header.css';
 
+
 function App(props) {
   console.log(props)
   return (
     <div className="App">
+      <Header />
+      <Main movies={props.movies}/>
+    </div>
+  );
+}
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
       <header>
-        <section>
-          <button classname='logout-button'>Log out, ya dingus</button>
-          <h1 className='welcome' id='welcome-msg'>Welcome, User!</h1>
-        </section>
-        <section id='logo'>
-          <img className='logo-pic' src={rtIcon} alt='The RT Logo'/>
-          <section className='header-text'>
-            <h1 className='site-title'>Rancid Tomatillos</h1>
-            <h3 className='quote'>'Some clever quote!'</h3>
-          </section>
-        </section>
-        <form>
-          <input placeholder='Find a Flick'/>
-        </form>
+        <UserInfo />
+        <Logo />
+        <SearchForm />
       </header>
+    );
+  }
+}
+
+class UserInfo extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <section>
+        <button classname='logout-button'>Log out, ya dingus</button>
+        <h1 className='welcome' id='welcome-msg'>Welcome, User!</h1>
+      </section>
+    )
+  }
+}
+
+class Logo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <section id='logo'>
+        <img className='logo-pic' src={rtIcon} alt='The RT Logo'/>
+        <section className='header-text'>
+          <h1 className='site-title'>Rancid Tomatillos</h1>
+          <h3 className='quote'>'Some clever quote!'</h3>
+        </section>
+      </section>
+    )
+  }
+}
+
+class SearchForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <form>
+        <input placeholder='Find a Flick'/>
+      </form>
+    )
+  }
+}
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: this.props.movies
+    }
+
+  }
+
+  render() {
+    return (
       <main>
         <h1 className='' id='main-title'>Movies</h1>
         <section id='movie-display'>
-          {props.movies.map(movie => <MovieCard key={movie.title} movie={movie}/>)}
+          {this.state.movies.map(movie => <MovieCard key={movie.title} movie={movie}/>)}
         </section>
       </main>
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
--- src/MovieCard.js
+++ src/MovieCard.js
@@ -3,28 +3,24 @@ import './App.css';
 
 class MovieCard extends React.Component{
   constructor(props) {
-    super();
-    let movie = props.movie;
+    super(props);
     this.state = {
-      poster: movie['poster_path'],
-      title: movie['title'],
-      globalRating: movie['average_rating']
+      poster: this.props.movie['poster_path'],
+      title: this.props.movie['title'],
+      globalRating: this.props.movie['average_rating']
     };
   }
 
   render() {
     return (
       <>
-        <div className='poster'>
-          <img className='poster-img' src={this.state.poster} alt={`Movie Poster for ${this.state.title}`}/>
-          <h6 className='global-rating'>{this.state.globalRating}</h6>
-        </div>
+        <h6>{this.state.globalRating}</h6>
+        <h6>URate</h6>
+        <img src={this.state.poster} alt={`Movie Poster for ${this.state.title}`}/>
+        <h4>{this.state.title}</h4>
       </>
     )
   }
 }
 
 export default MovieCard;
-
-// <h4 classname='movie-title'>{this.state.title}</h4>
-// <h6 classsName='user-rating'>00</h6>
