093c9181df7de539044879eaa4c99b608df956ff
Matthew Lane <themplane@gmail.com>
2020-10-06 17:25:20 -0700

    Create api-requests file and movie fetch. Coordinate Skeleton to dynamically accomodate all fetched movies.

--- src/App.js
+++ src/App.js
@@ -3,27 +3,28 @@ import rtIcon from './tomatillo-icon.png';
 import './App.css';
 import MovieCard from './MovieCard.js';
 
-function App() {
+function App(props) {
+  console.log(props)
   return (
     <div className="App">
       <header>
         <section>
-          <h1 class='' id='welcome-msg'>Welcome, User!</h1>
+          <h1 className='' id='welcome-msg'>Welcome, User!</h1>
           <button>Log out, ya dingus</button>
         </section>
         <section id='logo'>
           <img src={rtIcon} alt='The RT Logo'/>
-          <h1 class=''>Rancid Tomatillos</h1>
-          <h3 class=''>'Some clever quote!'</h3>
+          <h1 className=''>Rancid Tomatillos</h1>
+          <h3 className=''>'Some clever quote!'</h3>
         </section>
         <form>
           <input placeholder='Find a Flick'/>
         </form>
       </header>
       <main>
-        <h1 class='' id='main-title'>Movies</h1>
+        <h1 className='' id='main-title'>Movies</h1>
         <section id='movie-display'>
-          <MovieCard />
+          {props.movies.map(movie => <MovieCard key={movie.title} movie={movie}/> )}
         </section>
       </main>
     </div>
