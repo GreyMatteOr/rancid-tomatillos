f762acf5b7e66ef173f60bb545e3a932bc71fbf7
Brigette Doelp <brigette.doelp@gmail.com>
2020-10-11 19:18:50 -0600

    Test user info functionality

--- src/UserInfo/UserInfo.test.js
+++ src/UserInfo/UserInfo.test.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import MovieCard from '../MovieCard/MovieCard.js';
 import Header from '../Header/Header.js';
-import App from '../App.js';
 import Main from '../Main/Main.js';
 import UserInfo from './UserInfo/UserInfo.js';
+import App from '../App.js';
 import request from '../api-requests.js';
 
 
@@ -17,17 +17,15 @@ describe( 'UserInfo', () => {
     expect(screen.getByRole('header', { name: 'Please log in!' }))
   })
 
-  // it('should allow for a user to log out', () => {
-  //   let movies =[];
-  //   render(<App movies={movies} debug={true}/>);
-  //
-  //
-  // })
-  //
-  // it('should display user\'s email upon login', () => {
-  //   let movies =[];
-  //   render(<App movies={movies} debug={true}/>);
-  //
-  // })
+  it ('should allow for a user to log out', () => {
+    let movies =[];
+    render(<App movies={movies} debug={true}/>);
+
+    userEvent.type(screen.getByPlaceholderText('user id'), 'tinsel@turing.io');
+    userEvent.type(screen.getByPlaceholderText('password'), 'zxcvb');
+    userEvent.click(screen.getByRole('button', { name: 'Submit' }));
+
+    expect(screen.getByRole('header', { name: 'tinsel@turing.io' }))
+  })
 
 });
