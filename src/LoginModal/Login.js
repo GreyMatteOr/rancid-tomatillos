4de0defba2aa9912260afe70a15afa7d616ebc2f
Brigette Doelp <brigette.doelp@gmail.com>
2020-10-15 17:09:27 -0600

    Create filepath for login modal window

---
+++ src/LoginModal/Login.js
@@ -0,0 +1,14 @@
+// <input
+//   className="username"
+//   type="text"
+//   placeholder="user id"
+//   onChange={ (event) => this.state.userID = event.target.value }
+// />
+// <input
+//   className="password"
+//   type="password"
+//   placeholder="password"
+//   onChange={ (event) => this.state.password = event.target.value }
+// />
+// <button className="submit-credentials" id="submit-credentials" onClick={event => this.logIn(event)} submit=''>Submit</button>
+// <h3 id='warning'>{ this.state.isRejected ? 'TRY AGAIN FOREVER' : '' }</h3>
